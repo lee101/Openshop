72a435f73efb39f84fefeaa56039913cf6f6241b
Lee Penkman <leepenkman@gmail.com>
2026-03-23 02:57:08 +0000

    Cover custom program names in main input-size startup
    @codex-infinite codex-infinity.com

--- tests/main_usage_smoke.c
+++ tests/main_usage_smoke.c
@@ -391,6 +391,13 @@ int main(void) {
     }
 
     reset_app_state(0, NULL, 0, 0, stderr_text);
+    char *argv_custom_program_input_size[] = {"./bin/openshop-dev", "art/custom.png", "320", "240"};
+    if (!capture_main_stderr(4, argv_custom_program_input_size, stderr_text, sizeof(stderr_text), &exit_code) ||
+        !expect_successful_run("custom_program_input_size", exit_code, 0, "art/custom.png", 320, 240, stderr_text, "")) {
+        return 1;
+    }
+
+    reset_app_state(0, NULL, 0, 0, stderr_text);
     char *argv_input_size[] = {"openshop", "art/scene.png", "320", "240"};
     if (!capture_main_stderr(4, argv_input_size, stderr_text, sizeof(stderr_text), &exit_code) ||
         !expect_successful_run("input_size", exit_code, 0, "art/scene.png", 320, 240, stderr_text, "")) {
